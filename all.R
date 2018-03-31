0ba09ba43fde6167e2505adc5d58f5fc46a80e2c
vagrant <vagrant@mjk.local.com>
2018-03-31 08:38:12 -0400

    Finish organization, add gitignore, add CS PDF for convenient reference

---
+++ all.R
@@ -0,0 +1,7 @@
+# This file is meant to run all of the code without any of the RMD stuff
+# to be used for testing and experimentation
+
+rm(list=ls())
+
+source('code/00_Setup.R')
+source('code/01_LoadData.R')
