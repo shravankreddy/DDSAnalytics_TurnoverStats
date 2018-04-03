1c2492943beea6d7cd83b0853707a3c3fa0f0863
vagrant <vagrant@mjk.local.com>
2018-04-02 23:46:50 -0400

    Start to do analysis

--- code/top3.R
+++ code/top3.R
@@ -1,6 +1,7 @@
 
 toolarge <- c()
 tlcount <- c()
+lessthan2 <- c()
 nmax <- length(names(hr_employee_attrition))
 for (colidx in 1:nmax) {
     colname <- names(hr_employee_attrition)[colidx]
@@ -10,6 +11,9 @@ for (colidx in 1:nmax) {
        toolarge <- c(toolarge, colidx)
        tlcount <- c(tlcount, ucount)
     }
+    if (ucount < 2) {
+      lessthan2 <- c(lessthan2, colidx)
+    }
 }
 
 nmax <- length(toolarge)
@@ -17,4 +21,11 @@ for (idx in 1:nmax) {
     colidx <- toolarge[idx]
     colname <- names(hr_employee_attrition)[colidx]
     cat( sprintf("Column index %d - %s has %d unique values\n", colidx, colname, tlcount[idx]) )
+}
+
+nmax <- length(lessthan2)
+for (idx in 1:nmax) {
+  colidx <- lessthan2[idx]
+  colname <- names(hr_employee_attrition)[colidx]
+  cat( sprintf("Column index %d - %s had less than 2 unique values\n", colidx, colname) )
 }
