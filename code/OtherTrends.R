3468829b90870e60ab72ffdb0e77d277e10199ec
Dmitry Andreev <DmitryAndreev10@gmail.com>
2018-04-10 23:18:16 -0500

    Add files via upload

--- code/OtherTrends.R
+++ code/OtherTrends.R
@@ -6,6 +6,8 @@ hr_employee_attrition <- read.csv(dataFile)
 # highly correlated and need to be focused on
 Numeric_Vars <- hr_employee_attrition[,sapply(hr_employee_attrition, is.integer)]
 
+library(dplyr)
+
 Numeric_Vars <- hr_employee_attrition %>%
   select(ï..Age, DailyRate, DistanceFromHome, HourlyRate, MonthlyIncome, MonthlyRate, 
          NumCompaniesWorked, PercentSalaryHike, YearsAtCompany, YearsInCurrentRole,
@@ -21,7 +23,7 @@ corrplot(cor(Numeric_Vars), method = "number", type = "upper", order = "hclust",
 
 # Use corrplot as starting point / guideline for looking at
 # several variables against attrition to see if there is a trend
-
+library(ggplot2)
 library(gridExtra)
 theme_set(theme_light())
 
@@ -72,7 +74,6 @@ grid.arrange(Business_Travel, Department,
              Marital_Status, Over_Time,
              nrow = 3)
 
-
 Hourly_Rate <- ggplot(hr_employee_attrition, aes(x = Attrition, y = HourlyRate)) +
                geom_boxplot() + coord_flip()
 
@@ -102,3 +103,146 @@ Distance_Home_DensityPlot <- ggplot(hr_employee_attrition, aes(x = DistanceFromH
 grid.arrange(Distance_Home_BarPlot,
              Distance_Home_DensityPlot,
              nrow = 2)
+
+# Look into education variable. Need to rename numerical factor
+# levels to the labels provided under the data definition tab
+library(forcats)
+
+Education_Level <- hr_employee_attrition %>% 
+  mutate(Education = factor(Education)) %>% 
+  mutate(Education = fct_recode(Education, 
+                                "Below College" = "1", 
+                                "College" = "2", 
+                                "Bachelor" = "3", 
+                                "Master" = "4", 
+                                "Doctor" = "5"))
+library(RColorBrewer)
+theme_set(theme_light())
+
+ggplot(Education_Level, aes(Education, fill = Attrition)) + 
+  geom_bar(stat = "count", aes(y = ..count..), position = position_dodge()) +
+  theme(legend.position = "right", plot.title = element_text(hjust = 0.5, size = 16), axis.text.x = element_text(angle=90)) + 
+  labs(x = "Education Level", y = "Count", title = "Trend of Attrition with Education Level")
+
+ggplot(Education_Level, aes(Education, fill = Attrition)) + 
+  geom_bar(stat = "count", aes(y = ..count..), position = position_dodge()) +
+  theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5, size = 16), axis.text.x = element_text(angle=90)) + 
+  labs(x = "Education Level", y = "Count", title = "Education levels and field of education") +
+  facet_grid(~ EducationField)
+
+# Compare attrition between different levels of job satisfaction
+Job_Satisfaction_Level <- hr_employee_attrition %>% 
+  mutate(JobSatisfaction = factor(JobSatisfaction)) %>% 
+  mutate(JobSatisfaction = fct_recode(JobSatisfaction,
+                                      "Low" = "1",
+                                      "Medium" = "2",
+                                      "High" = "3",
+                                      "Very High" = "4"))
+
+ggplot(Job_Satisfaction_Level, aes(fill = Attrition, x = JobSatisfaction)) + 
+  geom_bar(position = 'fill')
+  
+ggplot(Job_Satisfaction_Level, aes(x = JobSatisfaction, group = Attrition)) + 
+  geom_bar(stat = "count", aes(y = ..prop.., fill = factor(..x..))) +
+  labs(x = "Job Satisfaction", y = "Percentage", title = "Job Satisfaction Vs Attrition Rates") + 
+  facet_wrap(~Attrition) +
+  theme(legend.position = "none", plot.title = element_text(hjust=0.5, size=14)) + 
+  geom_text(aes(label = scales::percent(..prop..), y= ..prop..), stat = "count", vjust = -0.5)
+
+# Compare job satisfaction among education levels between attrition
+# or not attrition groups  
+Education_Level %>%
+  mutate(JobSatisfaction = factor(JobSatisfaction)) %>% 
+  mutate(JobSatisfaction= fct_recode(JobSatisfaction,
+                                     "Low" = "1",
+                                     "Medium" = "2",
+                                     "High" = "3",
+                                     "Very High" = "4")) %>%
+  ggplot(aes(Education, fill = JobSatisfaction)) + 
+  geom_bar(position = 'fill') +
+  facet_wrap(~ Attrition)
+  
+Education_Level %>%
+  mutate(JobSatisfaction = factor(JobSatisfaction)) %>% 
+  mutate(JobSatisfaction= fct_recode(JobSatisfaction,
+                                     "Low" = "1",
+                                     "Medium" = "2",
+                                     "High" = "3",
+                                     "Very High" = "4")) %>%
+  ggplot(aes(Education, fill = JobSatisfaction)) + 
+  geom_bar(stat = "count", position = position_dodge()) + 
+  facet_wrap(~ Attrition) + 
+  theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5, size = 16), axis.text.x = element_text(angle = 90, hjust = 0.5)) + 
+  labs(x = "Education", y = "Satisfaction Count", title = "Comparing attrition with Education")
+
+# Compare attrition rate based on performance rating
+Performance_Rating <- hr_employee_attrition %>% 
+  mutate(PerformanceRating = factor(PerformanceRating)) %>% 
+  mutate(PerformanceRating = fct_recode(PerformanceRating,
+                                        "Low" = "1",
+                                        "Good" = "2",
+                                        "Excellent" = "3",
+                                        "Outstanding" = "4"))
+
+ggplot(Performance_Rating, aes(fill = Attrition, x = PerformanceRating)) + 
+  geom_bar(position = 'fill')
+
+# Compare attrition rate based on relationship satisfaction
+Relationship_Satisfaction <- hr_employee_attrition %>% 
+  mutate(RelationshipSatisfaction = factor(RelationshipSatisfaction)) %>% 
+  mutate(RelationshipSatisfaction = fct_recode(RelationshipSatisfaction,
+                                               "Low" = "1",
+                                               "Medium" = "2",
+                                               "High" = "3",
+                                               "Very High" = "4"))
+
+ggplot(Relationship_Satisfaction, aes(fill = Attrition, x = RelationshipSatisfaction)) + 
+  geom_bar(position = 'fill')
+
+# Compare attrition rate based work-life balance
+Work_Life_Balance <- hr_employee_attrition %>% 
+  mutate(WorkLifeBalance = factor(WorkLifeBalance)) %>% 
+  mutate(WorkLifeBalance = fct_recode(WorkLifeBalance,
+                                      "Bad" = "1",
+                                      "Good" = "2",
+                                      "Better" = "3",
+                                      "Best" = "4"))
+
+ggplot(Work_Life_Balance, aes(fill = Attrition, x = WorkLifeBalance)) + 
+  geom_bar(position = 'fill')
+
+# Compare attrition rate based on Environment Satisfaction
+Environment_Satisfaction <- hr_employee_attrition %>% 
+  mutate(EnvironmentSatisfaction = factor(EnvironmentSatisfaction)) %>% 
+  mutate(EnvironmentSatisfaction = fct_recode(EnvironmentSatisfaction,
+                                              "Low" = "1",
+                                              "Medium" = "2",
+                                              "High" = "3",
+                                              "Very High" = "4"))
+
+ggplot(Environment_Satisfaction, aes(fill = Attrition, x = EnvironmentSatisfaction)) + 
+  geom_bar(position = 'fill')
+
+# Compare attrition rate based on Job Involvement
+Job_Involvement <- hr_employee_attrition %>% 
+  mutate(JobInvolvement = factor(JobInvolvement)) %>% 
+  mutate(JobInvolvement = fct_recode(JobInvolvement,
+                                     "Low" = "1",
+                                     "Medium" = "2",
+                                     "High" = "3",
+                                     "Very High" = "4"))
+
+ggplot(Job_Involvement, aes(fill = Attrition, x = JobInvolvement)) + 
+  geom_bar(position = 'fill')
+
+
+# Compare attrition rate based overtime
+ggplot(hr_employee_attrition, aes(x = OverTime, group = Attrition)) +
+  geom_bar(stat = "count", aes(y = ..prop.., fill = factor(..x..))) +
+  labs(x = "Overtime", y = "Percentage", title = "Overtime Vs Attrition Rates") +
+  facet_wrap(~Attrition) + 
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5, size = 14)) +
+  geom_text(aes(label = scales::percent(..prop..), y = ..prop..), stat = "count", vjust = -0.5)
+
+ggplot(hr_employee_attrition, aes(fill = Attrition, x = OverTime)) + 
+  geom_bar(position = 'fill')
