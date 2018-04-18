8a87772b1d7948b19b2183339e006c5b2d195102
drcrod <carollr@smu.edu>
2018-04-18 00:11:56 -0500

    .R update

--- code/Model.R
+++ code/Model.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(knitr)
 library(randomForest)
 library(caret)
+library(party)
+
 dataFileCsv <- "../data/CaseStudy2data.csv"
 hr_employee_attrition <- read.csv(dataFileCsv)
 attach(hr_employee_attrition)
@@ -12,43 +14,68 @@ cat( sprintf("Remove near zero value columns: %s\n", uniq) )
 hr_reduced <- hr_employee_attrition[,-nzv]
 detach(hr_employee_attrition)
 names(hr_reduced)[1] <- "Age"
-attach(hr_reduced)
+set.seed(402)
+ind <- sample(2, nrow(hr_reduced), replace = TRUE, prob = c(0.7, 0.3))
+train <- hr_reduced[ind==1,]
+test <- hr_reduced[ind==2,]
 samplesize <- (nrow(hr_reduced))*.70
 datasize <- nrow(hr_reduced)
-train=sample(1:datasize,samplesize)
 features <- as.numeric(ncol(hr_reduced)-1)
 
 hr_reduced.rf <- randomForest(Attrition ~ . , data = hr_reduced , subset = train)
 hr_reduced.rf
 
+#PFigure 1
+plot(hr_reduced.rf, main = "RandomForest - Error vs Trees Chart")  
+set.seed(222)
+rf <- randomForest(Attrition~., data=train,
+                   ntree = 300,
+                   mtry = 10,
+                   importance = TRUE,
+                   proximity = TRUE)
+print(rf)
+attributes(rf)
 
-plot(hr_reduced.rf, main = "RandomForest - Error vs Trees Chart")    
-oob.err=double(as.numeric(features))
-test.err = double(as.numeric(features))
+# Prediction & Confusion Matrix - train data
+library(caret)
+p1 <- predict(rf, train)
+confusionMatrix(p1, train$Attrition)
 
-#mtry is no of Variables randomly chosen at each split
-for(mtry in 1:as.numeric(features))
-{
-  rf=randomForest(Attrition ~ . , data = hr_reduced , subset = train,mtry=mtry,ntree=500) 
-  oob.err[mtry] = rf$mse[500] #Error of all Trees fitted
-  
-  pred<-predict(rf,hr_reduced[-train,]) #Predictions on Test Set for each Tree
-  test.err[mtry]= with(hr_reduced[-train,], mean( (Attrition - pred)^2)) #Mean Squared Test Error
-  
-  cat(mtry," ") #printing the output to the console
-}
-test.err
-oob.err
-matplot(1:mtry , cbind(oob.err,test.err), pch=19 , col=c("red","blue"),type="b",ylab="Mean Squared Error",xlab="Number of Predictors Considered at each Split")
-legend("topright",legend=c("Out of Bag Error","Test Error"),pch=19, col=c("red","blue"))
-hr_reduced.lm <- glm(relevel(as.factor(Attrition),ref="Yes") ~ relevel(as.factor(JobRole),ref="Research Scientist") + relevel(as.factor(OverTime),ref="Yes") + DistanceFromHome, data = hr_reduced, family =binomial())
+#  Prediction & Confusion Matrix - test data
+p2 <- predict(rf, test)
+confusionMatrix(p2, test$Attrition)
 
-summary(hr_reduced.lm)
+#Error Rate of Random Forest
+plot(rf)
 
-df.res <- resid(hr_reduced.lm)
-df.res
-plot(df.res, ylab = "Residuals", main = "Residuals - Age ~ JobRole + OverTime + DistanceFromHome")
+# No. of nodes for the trees
+hist(treesize(rf),
+     main = "No. of Nodes for the Trees",
+     col = "Blue")
+# Tune mtry
+t <- tuneRF(train[,-2], train[,2],
+            stepFactor = 0.5,
+            plot = TRUE,
+            ntreeTry = 300,
+            trace = TRUE,
+            improve = 0.05)
 
+# Partial Dependence Plot
+partialPlot(rf, train, Attrition, "Yes")
 
-detach(hr_reduced)
-detach(hr_reduced.rf)
+# Extract Single Tree
+getTree(rf, 150, labelVar = TRUE)
+
+# Multi-dimensional Scaling Plot of Proximity Matrix
+MDSplot(rf, train$Attrition)
+
+#Decision tree with party
+library(party)
+mytree <- ctree(Attrition~., hr_reduced, controls=ctree_control(mincriterion=0.9, minsplit=50))
+print(mytree)
+plot(mytree,type="simple")
+
+#Misclassification error
+tab<-table(predict(mytree), mydata$Attrition)
+print(tab)
+1-sum(diag(tab))/sum(tab)
